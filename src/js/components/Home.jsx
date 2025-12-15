dd83c60ad150f86868ec1eff53f025eb966ca6c6
alejandrolobatogil-dot <alejandrolobatogil@gmail.com>
2025-12-15 10:15:32 +0000

    finalizado1.0

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,40 @@
 import React from "react";
+import { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	const [text, setText] = useState("")
+	const [array, setArray] = useState([])
+
+    const crearLista = () =>{
+		if (text.trim() === "") return;
+		setArray(prev => [...prev, text])
+		setText("");
+	}
+		
+	return (
+		<div className="container text-center  width=14rem height=14rem ">
+			<h1>Crear Tareas</h1>
+			<input
+			type="text"
+			value={text}
+			onChange={(e) => setText(e.target.value)}
+			/>	
+
+			<button onClick={() => crearLista()}>Agregar</button>
+
+				{array.map((item, index)=>{
+					return(
+						<div key={index}>
+							<span>{index +1}. {item}</span>
+						</div>
+					)
+				})
+			}
 		</div>
 	);
 };
