4d613bc465d79465b519fa318e05c1f8c280d1f9
alejandrolobatogil-dot <alejandrolobatogil@gmail.com>
2025-12-30 11:05:50 +0000

    Mejorada:)

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -10,31 +10,46 @@ const Home = () => {
 	const [text, setText] = useState("")
 	const [array, setArray] = useState([])
 
-    const crearLista = () =>{
+	const crearLista = () => {
 		if (text.trim() === "") return;
 		setArray(prev => [...prev, text])
 		setText("");
 	}
-		
+	const añadirEnter = (e) => {
+		if (e.key === "Enter") {
+			crearLista();
+		}
+	};
+
+	const borrarTexto = (index) => {
+		setArray((prev) => prev.filter((_, i) => i !== index));
+	};
+
 	return (
-		<div className="container text-center  width=14rem height=14rem ">
-			<h1>Crear Tareas</h1>
-			<input
-			type="text"
-			value={text}
-			onChange={(e) => setText(e.target.value)}
-			/>	
+		<div className="container text-center" style={{ width: "14rem", height: "14rem" }}>
+			<div className="titulo">
+				<h1>Crear Tareas</h1>
 
-			<button onClick={() => crearLista()}>Agregar</button>
+				<div>
+					<input
+						type="text"
+						value={text}
+						onChange={(e) => setText(e.target.value)}
+						onKeyUp={añadirEnter} />
 
-				{array.map((item, index)=>{
-					return(
-						<div key={index}>
-							<span>{index +1}. {item}</span>
+				</div>
+				<button onClick={() => crearLista()}>Agregar</button>
+				{array.length === 0 ? (<p><strong>No hay tareas pendientes</strong></p>) : (
+					array.map((item, index) => (
+						<div key={index} className="task-item">
+							<span>{index + 1}. {item}</span>
+							<button
+								className="delete-btn"
+								onClick={() => borrarTexto(index)}>×</button>
 						</div>
-					)
-				})
-			}
+					))
+				)}
+			</div>
 		</div>
 	);
 };
